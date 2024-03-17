d4fcc59b7569af4d973634b3bcfb302df6077301
jayellul <jellul146@gmail.com>
2024-03-16 23:28:39 -0400

    wip

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+import styles from "./header.module.css";
+
+const Header = () => {
+  return (
+    <div className={styles.wrapper}>
+      <header className={styles.container}>
+        <Link href="/">Jayla ellul</Link>
+        <nav className={styles.nav}>
+          <Link href="/about">About</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/projects">Projects</Link>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
